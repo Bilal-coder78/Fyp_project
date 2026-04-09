d75a34c24b10b2d83d9f34ce48809c7bf6eee3dc
M Bilal <bilalsikander87@gmail.com>
2026-04-09 16:58:20 +0500

    Improves error and loading state UI for login flow.

--- frontend/src/routes/protectedRoute.jsx
+++ frontend/src/routes/protectedRoute.jsx
@@ -4,7 +4,15 @@ import { useAuth } from "../context/authContext.jsx";
 export default function ProtectedRoute({ children }) {
   const { user, loading } = useAuth();
 
-  if (loading) return <p>Loading...</p>;
+  if (loading) {
+    return (
+      <div className="flex items-center justify-center min-h-screen">
+        <p className="text-green-500 text-4xl font-semibold">
+          Loading...
+        </p>
+      </div>
+    );
+  }
 
   if (!user) return <Navigate to="/login" />;
 
